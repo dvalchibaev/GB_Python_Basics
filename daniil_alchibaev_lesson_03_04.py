275616515bd791bce891152ea7c22b92b59cf3b8
dvalchibaev <56756943+dvalchibaev@users.noreply.github.com>
2021-05-14 20:40:22 +0300

    Update daniil_alchibaev_lesson_03_04.py

--- daniil_alchibaev_lesson_03_04.py
+++ daniil_alchibaev_lesson_03_04.py
@@ -9,9 +9,11 @@ def thesaurus_adv(*args):
         if not thesaurus_dict.get(last_name[0]):
             # если на данную букву фамилии словарь еще не заводился, то создаем новый
             thesaurus_dict[last_name[0]] = {first_name[0]: name}
+        elif thesaurus_dict[last_name[0]].get(first_name[0]):
+            # увидел ошибку во время вебинара, исправил кривовато, но чтобы было минимум красного
+            thesaurus_dict[last_name[0]][first_name[0]].append(name)
         else:
-            # иначе обновляем словарь
-            thesaurus_dict[last_name[0]].update({first_name[0]: name})
+            thesaurus_dict[last_name[0]].setdefault(first_name[0], [])
     return thesaurus_dict
 
 

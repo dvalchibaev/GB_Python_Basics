db5f7d3506bcb7fac980999d306aeb93773ee2c6
dvalchibaev <56756943+dvalchibaev@users.noreply.github.com>
2021-06-05 20:27:53 +0300

    Update daniil_alchibaev_08_04.py

--- daniil_alchibaev_08_04.py
+++ daniil_alchibaev_08_04.py
@@ -1,14 +1,19 @@
-# доделаю после урока
+def val_checker(callback):
+    def _val_checker(func):
+        def wrapper(*argv):
+            if callback(*argv):
+                result = func(*argv)
+                return result
+            else:
+                raise ValueError(f'Wrong input: {argv}')
+        return wrapper
+    return _val_checker
 
-def val_checker(fun):
-    def wrapper(*argv):
-        return fun(*argv)
-    return wrapper
 
-
-@val_checker
+@val_checker(lambda x: x > 0)
 def calc_cube(num):
     return num ** 3
 
 
 print(calc_cube(3))
+print(calc_cube(-3))

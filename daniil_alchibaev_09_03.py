3719b208d4d9ff4c9dbf7deb3e616b3635150443
dvalchibaev <56756943+dvalchibaev@users.noreply.github.com>
2021-06-11 16:38:00 +0300

    Update daniil_alchibaev_09_03.py

--- daniil_alchibaev_09_03.py
+++ daniil_alchibaev_09_03.py
@@ -1,11 +1,19 @@
-# заполню 09.06
 class Worker:
-    _first_name, _last_name = 'John', 'Doe'
-    _position = "unknown"
+    def __init__(self, first_name, last_name, position, wage, bonus):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
 
-    def __init__(self):
-        pass
+
+class Position(Worker):
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_total_income(self):
+        return f'{self._income["wage"] + self._income["bonus"]}'
 
 
 if __name__ == '__main__':
-    pass
+    ceo_position = Position('Jeff', 'Bezos', 'CEO', 1e7, 6.5e7)
+    print(ceo_position.get_total_income())
